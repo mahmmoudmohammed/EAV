ff81039152d99ec90fa37ce58f078821b0f09df3
mahmoud-sharqawi <mahmoud.mohamed@mts-egy.com>
2025-03-03 04:54:55 +0200

    enhance

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,12 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Http\Domains\User\Model\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Http\Domains\User\Model\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
