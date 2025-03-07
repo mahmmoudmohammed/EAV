1763f37e04f3f3abe9f7c20559946f7c5df7f44d
mahmmoudmohammed <mahmoud.mohamed.fci1@gmail.com>
2025-03-07 05:25:02 +0200

    Feat:Configure passport

---
+++ app/Http/Domains/User/Model/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Domains\User\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable, HasApiTokens;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+}
---
+++ app/Http/Domains/User/UserServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Domains\User;
+
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        Passport::tokensExpireIn(now()->addDays(10));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(3));
+    }
+}
